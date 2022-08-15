fc68e6e8ed87dabd99829a2a50caa53af38cdf26
Prasad <banprasad21@gmail.com>
2022-08-16 01:07:53 +0530

    [Raghav] Add: Harmonic Number problem

---
+++ Basic_Core_Problems/HarmonicNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Problems
+{
+    internal class HarmonicNumber
+    {
+        public void HarmoncValue()
+        {
+            Console.WriteLine("Enter number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n <= 0)
+            {
+                Console.WriteLine("Invalid number enter again");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            float result = 0;
+
+            for (float i = 1; i <= n; i++)
+            {
+                result += 1 / i;
+            }
+            Console.WriteLine("Hormonic result is " + result);
+        }
+    }
+}
--- Basic_Core_Problems/Program.cs
+++ Basic_Core_Problems/Program.cs
@@ -9,6 +9,8 @@
             Console.WriteLine("Enter a number to select Problem");
             Console.WriteLine("1.Coin Flip Problem");
             Console.WriteLine("2.Leap Year Problem");
+            Console.WriteLine("3. Power Of Two");
+            Console.WriteLine("4. Harmonic Number");
 
             int ip = Convert.ToInt32(Console.ReadLine());
             switch (ip)
@@ -24,6 +26,10 @@
                 case 3:
                     PowerOfTwo power = new PowerOfTwo();
                     power.PowerTwo();
+                    break;
+                case 4:
+                    HarmonicNumber harmonicNumber = new HarmonicNumber();
+                    harmonicNumber.HarmoncValue();
                     break;
 
 
