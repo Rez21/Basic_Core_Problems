1ba4695ebe898d8af10c34beed1fe3099bf412ee
Rez21 <banprasad21@gmail.com>
2022-08-15 07:33:14 +0530

    [Raghav] Add: Leap year problem

--- Basic_Core_Problems/Program.cs
+++ Basic_Core_Problems/Program.cs
@@ -7,12 +7,30 @@
 
         static void Main(string[] args)
         {
-            //coin flip problem
-            Coin_Flip toss = new Coin_Flip();
-            toss.CoinFlip();
+            Console.WriteLine("Basic Core Problems");
+            Console.WriteLine("Enter a number to select Problem");
+            Console.WriteLine("1.Coin Flip Problem");
+            Console.WriteLine("2.Leap Year Problem");
+
+            int ip = Convert.ToInt32(Console.ReadLine());
+
+            switch (ip)
+            {
+                case 1:
+                    Coin_Flip toss = new Coin_Flip();
+                    toss.CoinFlip();
+                    break;
+                case 2:
+                    Leap_Year year = new Leap_Year();
+                    year.LeapYearCheck();
+                    break;
+
+            }
 
         }
-
-
     }
+
 }
+
+
+   
