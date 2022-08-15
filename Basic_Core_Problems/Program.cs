4ab06ef3f594e4ef49c7438200308fca0d5219e3
Rez21 <banprasad21@gmail.com>
2022-08-15 07:18:56 +0530

    [Raghav] Add: Coin Flip problem

---
+++ Basic_Core_Problems/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Basic_Core_Problems {
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //coin flip problem
+            Coin_Flip toss = new Coin_Flip();
+            toss.CoinFlip();
+
+        }
+
+
+    }
+}
